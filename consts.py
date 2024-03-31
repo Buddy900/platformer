7912f8559d49aefd9c2bd0dc2fbe7f6a8baceaab
ISAAC BABISTER <ISAAC.BABISTER@yahoo.com>
2024-03-31 23:33:30 +0100

    changed things a bit

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 WIDTH, HEIGHT = 1000, 800
 
-FPS = 60
+FPS = 120
 
 HELP_MESSAGE = """\
 HELP MENU
@@ -14,4 +14,7 @@ M changes modes:
 - Platformer mode lets you control the player
 - Edit mode lets you click to place platforms
     - You can still move around (hold shift to move faster)
+    
+F1 starts recording coordinates of the player in a list
+F2 prints some info about that list
 """
